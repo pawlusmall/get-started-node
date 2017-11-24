fe9199c86faa1413833026d9cbcd11920cb89834
Pablo Soto <pablo.soto@neosentec.com>
2017-11-24 13:27:29 +0100

    skeleton and some fixes

--- views/js/goc2017.js
+++ views/js/goc2017.js
@@ -1,4 +1,4 @@
-var camera, scene, renderer, geometry, material, mesh, mixer, clock, container;
+var camera, scene, renderer, geometry, material, mesh, skeleton, mixer, clock, controls;
 
 init();
 animate();
@@ -6,14 +6,11 @@ animate();
 function init() {
 
     scene = new THREE.Scene();
-
     clock = new THREE.Clock();
 
-    container = document.getElementById( 'container' );
-
     camera = new THREE.PerspectiveCamera( 25, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.set( 0, 3, 20 );
-    scene.add(camera);
+    camera.position.set( 0, 0, 10 );
+    scene.add(camera);    
 
     var ambientLight = new THREE.AmbientLight( 0xffffff, 0.2 );
     scene.add( ambientLight );
@@ -26,22 +23,40 @@ function init() {
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
 
-    container.appendChild(renderer.domElement);
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
+    controls.addEventListener( 'change', render );
+
+    document.body.appendChild(renderer.domElement);
     window.addEventListener( 'resize', onWindowResize, false );
 
     loadModel();
 }
 
-function loadModel() {
+function loadModel() {    
+
     var loader = new THREE.ColladaLoader();
     loader.load('models/stormtropper.dae', function ( collada ) {
-        var avatar = collada.scene;
-        avatar.rotation.z = 3;
+        collada.scene.traverse(child => {
+            if (child instanceof THREE.SkinnedMesh) {
+                mesh = child;             
+            }
+        });
+
+        mesh.rotation.z = 3;
+
+        skeleton = new THREE.SkeletonHelper(mesh);
+        skeleton.visible = true;
+        scene.add(skeleton);
+        
         var animations = collada.animations;
-        scene.add(avatar);       
-        mixer = new THREE.AnimationMixer( avatar );
-        var action = mixer.clipAction( animations[ 0 ] ).play();
-        console.log("loaded!");        
+        mixer = new THREE.AnimationMixer(mesh);        
+        //var action = mixer.clipAction(animations[ 0 ]).play(); 
+        scene.add(mesh);      
+        
+        mesh.skeleton.bones[0].position.x = 1;
+        mesh.skeleton.bones[3].position.y = 2;
+        mesh.skeleton.bones[10].position.x = 1;
+        mesh.skeleton.bones[20].position.y = 2;
     });
 
 }
@@ -55,18 +70,15 @@ function onWindowResize() {
     
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    
     renderer.setSize( window.innerWidth, window.innerHeight );
     
 }
 
 function render() {    
-
     var delta = clock.getDelta();
     
     if ( mixer !== undefined ) {    
         mixer.update( delta );
     }
     renderer.render(scene, camera);  
-
 }
