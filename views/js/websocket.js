4e96b8c90e1175a244db0dbd81da487c685e6c9e
Jorge López <nokutu@openmailbox.org>
2017-11-24 12:52:50 +0100

    Socket

--- views/js/websocket.js
+++ views/js/websocket.js
@@ -3,3 +3,6 @@ let socket = new WebSocket('ws://interfacehackaton-subtetanical-myxomatous.myblu
 socket.onmessage = event => {
     console.log("Message received: " + event.data)
 };
+socket.onopen = function (event) {
+    console.log("Connected to socket")
+};
