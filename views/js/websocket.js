fb08f696d7a8551615d455bf574480427e495fe8
Jorge López <nokutu@openmailbox.org>
2017-11-24 12:21:34 +0100

    Websocket client

---
+++ views/js/websocket.js
@@ -0,0 +1,8 @@
+let socket = Websocket('ws://interfacehackaton-subtetanical-myxomatous.mybluemix.net:8080/');
+
+socket.onmessage = event => {
+    console.log("Message received: " + event.data)
+};
+socket.onopen = function (event) {
+    socket.send("Here's some text that the server is urgently awaiting!");
+};
