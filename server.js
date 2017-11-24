0c88b786fcf30b148330d4ebf9a1ba46f7f9ae2e
Jorge López <nokutu@openmailbox.org>
2017-11-24 12:34:01 +0100

    Added broadcast

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    ws.send('something');
+  ws.on('message', function incoming(data) {
+      wss.clients.forEach(function each(client) {
+          if (client !== ws && client.readyState === WebSocket.OPEN) {
+              client.send(data);
+          }
+      });
+      ws.send("OK");
   });
-
 });
 
 // parse application/x-www-form-urlencoded
